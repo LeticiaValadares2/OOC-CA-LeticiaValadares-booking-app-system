af8a04a2c7720ad47ffd65669d779990fabe42b3
leticiavaladares2 <2024389@student.cct.ie>
2026-07-24 10:56:53 +0100

    Created reservationNotFound and OverbookingException

---
+++ src/OverbookingException.java
@@ -0,0 +1,6 @@
+public class OverbookingException extends Exception {
+
+    public OverbookingException(String message) {
+        super(message);
+    }
+}
---
+++ src/ReservationNotFoundException.java
@@ -0,0 +1,6 @@
+public class ReservationNotFoundException extends Exception {
+
+    public ReservationNotFoundException(String message) {
+        super(message);
+    }
+}
